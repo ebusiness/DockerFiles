7805200d7413ed695ead966a462a9691a35b782f
Xinguang Wang <wangxinguang@e-business.co.jp>
2017-01-19 10:44:03 +0900

    sales

--- sales/enterpoint.sh
+++ sales/enterpoint.sh
@@ -4,39 +4,37 @@ if [[ ! -d /ebusiness ]]; then
   cd /ebusiness
   git clone https://github.com/ebusiness/ebusiness.git ./
 
-  if [ ${MYSQL_SERVER}x == ""x ]; then
-    MYSQL_SERVER=172.17.0.1
-  fi
-  echo "Mysql server:" ${MYSQL_SERVER}
-
-  FILENAME=/ebusiness/employee/settings.py
-  mv ${FILENAME} ${FILENAME}.bak
-  sed "s/'HOST'.*/'HOST':'${MYSQL_SERVER}',/g" ${FILENAME}.bak >${FILENAME}
-  rm ${FILENAME}.bak
-
-  if [ ${MYSQL_USER}x != ""x ]; then
-    mv ${FILENAME} ${FILENAME}.bak
-    sed "s/'USER'.*/'USER':'${MYSQL_USER}',/g" ${FILENAME}.bak >${FILENAME}
-    rm ${FILENAME}.bak
-  fi
-
-  if [ ${MYSQL_PASSWORD}x != ""x ]; then
-    mv ${FILENAME} ${FILENAME}.bak
-    sed "s/'PASSWORD'.*/'PASSWORD':'${MYSQL_PASSWORD}',/g" ${FILENAME}.bak >${FILENAME}
-    rm ${FILENAME}.bak
-  fi
-
-  if [ ${MYSQL_DATABASE}x != ""x ]; then
-    mv ${FILENAME} ${FILENAME}.bak
-    sed "s/'NAME'.*/'NAME':'${MYSQL_DATABASE}',/g" ${FILENAME}.bak >${FILENAME}
-    rm ${FILENAME}.bak
-  fi
-
 fi
 
 cd /ebusiness
 
+git checkout -- employee/settings.py
 git pull
+
+# settings
+
+if [ ${MYSQL_SERVER}x == ""x ]; then
+  MYSQL_SERVER=172.17.0.1
+fi
+echo "Mysql server:" ${MYSQL_SERVER}
+
+FILENAME=/ebusiness/employee/settings.py
+mv ${FILENAME} ${FILENAME}.bak
+sed "s/'HOST'.*/'HOST':'${MYSQL_SERVER}',/g" ${FILENAME}.bak >${FILENAME}
+rm ${FILENAME}.bak
+
+if [ ${MYSQL_USER}x != ""x ]; then
+  mv ${FILENAME} ${FILENAME}.bak
+  sed "s/'USER'.*/'USER':'${MYSQL_USER}',/g" ${FILENAME}.bak >${FILENAME}
+  rm ${FILENAME}.bak
+fi
+
+if [ ${MYSQL_PASSWORD}x != ""x ]; then
+  mv ${FILENAME} ${FILENAME}.bak
+  sed "s/'PASSWORD'.*/'PASSWORD':'${MYSQL_PASSWORD}',/g" ${FILENAME}.bak >${FILENAME}
+  rm ${FILENAME}.bak
+fi
+
 
 if [[ ! -d log ]]; then
   mkdir log
