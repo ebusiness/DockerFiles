3703ac420550d76ac2b6b471cf3556091704ed74
Xinguang Wang <wangxinguang@e-business.co.jp>
2017-01-26 19:28:46 +0900

    lego

--- tomo-server/standardalone.sh
+++ tomo-server/standardalone.sh
@@ -6,6 +6,12 @@ docker run -d --restart=always --name webhook -p 9000:9000 -v $(pwd)/config.json
 
 # lego
 
+# docker run -it --rm -p 80:80 -v /home/ec2-user/ssl/.lego:/.lego  xenolf/lego --email="admin@genbatomo.com" --domains="api.genbatomo.com" --domains="admin.genbatomo.com" --accept-tos run
+
+# docker run -it --rm -p 80:80 -v /home/ec2-user/ssl/.lego:/.lego  xenolf/lego --email="admin@genbatomo.com" --domains="api.genbatomo.com" --domains="admin.genbatomo.com" renew
+
+# ln -s /home/ec2-user/ssl/.lego/certificates /certificates
+
 # mongodb
 docker run -d --restart=always --name mongo -p 27017:27017 mvertes/alpine-mongo
 
@@ -15,5 +21,5 @@ docker run -d --restart=always --name redis -p 6379:6379 redis:alpine
 
 
 # tomo
-docker run -d --restart=always --name tomo -p 443:443 -v $(pwd)/global.js:/global.js -v $(pwd)/certificates:/certificates --link mongo:mongo --link redis:redis ebusinessdocker/tomo
-# docker run -it --rm --name tomo -p 443:443 -v $(pwd)/global.js:/global.js -v $(pwd)/certificates:/certificates --link mongo:mongo --link redis:redis ebusinessdocker/tomo sh
+docker run -d --restart=always --name tomo -p 443:443 -v $(pwd)/global.js:/global.js -v $(pwd)/certificates:/certificates -v $(pwd)/apn:/apn --link mongo:mongo --link redis:redis -e NODE_ENV=production ebusinessdocker/tomo
+# docker run -it --rm -p 443:443 -v $(pwd)/global.js:/global.js -v $(pwd)/certificates:/certificates -v $(pwd)/apn:/apn --link mongo:mongo --link redis:redis -e NODE_ENV=production ebusinessdocker/tomo sh
