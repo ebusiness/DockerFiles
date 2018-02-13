d1200b466aabd42baa8d67804d2a81e4098b3c55
YangWanjun <ywjsailor@gmail.com>
2018-02-13 20:03:17 +0900

    sales

--- sales/enterpoint.sh
+++ sales/enterpoint.sh
@@ -8,33 +8,9 @@ fi
 
 cd /ebusiness
 
-git checkout -- employee/settings.py
 git pull
 
 # settings
-
-if [ ${MYSQL_SERVER}x == ""x ]; then
-  MYSQL_SERVER=172.17.0.1
-fi
-echo "Mysql server:" ${MYSQL_SERVER}
-
-FILENAME=/ebusiness/employee/settings.py
-mv ${FILENAME} ${FILENAME}.bak
-sed "s/'HOST'.*/'HOST':'${MYSQL_SERVER}',/g" ${FILENAME}.bak >${FILENAME}
-rm ${FILENAME}.bak
-
-if [ ${MYSQL_USER}x != ""x ]; then
-  mv ${FILENAME} ${FILENAME}.bak
-  sed "s/'USER'.*/'USER':'${MYSQL_USER}',/g" ${FILENAME}.bak >${FILENAME}
-  rm ${FILENAME}.bak
-fi
-
-if [ ${MYSQL_PASSWORD}x != ""x ]; then
-  mv ${FILENAME} ${FILENAME}.bak
-  sed "s/'PASSWORD'.*/'PASSWORD':'${MYSQL_PASSWORD}',/g" ${FILENAME}.bak >${FILENAME}
-  rm ${FILENAME}.bak
-fi
-
 
 if [[ ! -d log ]]; then
   mkdir log
